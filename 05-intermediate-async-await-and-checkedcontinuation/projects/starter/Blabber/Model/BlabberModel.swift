c96de95e7f7c54eaa7dea94ebb14cdb3002c20fb
Rohan Bhale <rohanbhale@gmail.com>
2024-04-21 21:18:56 +0200

    feat(continuation closure callback): Convers closure callback to async await compatible code using continuation

--- 05-intermediate-async-await-and-checkedcontinuation/projects/starter/Blabber/Model/BlabberModel.swift
+++ 05-intermediate-async-await-and-checkedcontinuation/projects/starter/Blabber/Model/BlabberModel.swift
@@ -61,6 +61,22 @@ class BlabberModel: ObservableObject {
     print(location.description)
     manager.stopUpdatingLocation()
     delegate = nil
+    let address: String = try await withCheckedThrowingContinuation { continuation in
+      AddressEncoder.addressFor(location: location) { address, error in
+        switch (address, error) {
+        case (nil, let error?):
+          continuation.resume(throwing: error)
+        case (let address?, nil):
+          continuation.resume(returning: address)
+        case (nil, nil):
+          continuation.resume(throwing: "Address encoding failed")
+        case let (address?, error?):
+          continuation.resume(returning: address)
+          print(error)
+        }
+      }
+    }
+    try await say("📍 \(address)")
   }
 
   /// Does a countdown and sends the message.
