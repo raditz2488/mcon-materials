170b64dccfe910ba0770937af29002a5a5f4d627
Rohan Bhale <rohanbhale@gmail.com>
2024-04-28 15:14:59 +0200

    feat(timeout task): Creates TimeoutTask to timeout if task does not complete in given time

---
+++ 06-testing-asynchronous-code/projects/starter/BlabberTests/Utility/TimeoutTask.swift
@@ -0,0 +1,50 @@
+//
+//  TimeoutTask.swift
+//  BlabberTests
+//
+//  Created by Rohan Bhale on 28/04/24.
+//
+
+import Foundation
+
+class TimeoutTask<Success> {
+  let seconds: Int
+  let operation: @Sendable () async throws -> Success
+  var continuation: CheckedContinuation<Success, Error>?
+
+  init(seconds: Int, operation: @escaping @Sendable () -> Success) {
+    self.seconds = seconds
+    self.operation = operation
+  }
+
+  var value: Success {
+    get async throws {
+      try await withCheckedThrowingContinuation { continuation in
+        self.continuation = continuation
+        Task {
+          try await Task.sleep(for:.seconds(seconds))
+          self.continuation?.resume(throwing: TimeoutError())
+          self.continuation = nil
+        }
+        Task {
+          let result = try await self.operation()
+          self.continuation?.resume(returning: result)
+          self.continuation = nil
+        }
+      }
+    }
+  }
+
+  func cancel() {
+    continuation?.resume(throwing: CancellationError())
+    continuation = nil
+  }
+}
+
+extension TimeoutTask {
+  struct TimeoutError: LocalizedError {
+    var errorDescription: String? {
+      "The task timed out."
+    }
+  }
+}
